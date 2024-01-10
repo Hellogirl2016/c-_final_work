233e0f7772cfa7ea082b67541df15bafd179655b
Hellogirl2016 <av_arapova@mail.ru>
2024-01-10 10:39:08 -0500

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+void Filtration(string[] arr)
+{
+System.Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i].Length < 4)
+    {
+    System.Console.Write($"{arr[i] }");
+    }
+}
+System.Console.Write("]");
+}
+
+
+string[] arr = { "Russia", "Denmark", "Kazan", "Ufa"};
+Filtration(arr);
+
+
+
