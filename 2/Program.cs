a5e0dfba38eab16e6f2b60520f5470a4353ec4e1
Hellogirl2016 <av_arapova@mail.ru>
2024-01-10 11:07:10 -0500

    Add files via upload

---
+++ 2/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] words = { "Hello", "2", "world", ":-@"};
+  
+IEnumerable <string> query = from word in words  
+                             where word.Length < 4  
+                             select word;  
+  
+foreach (string str in query)  
+Console.WriteLine(str);  
